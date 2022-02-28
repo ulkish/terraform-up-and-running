37cc117be012b343462fe9e3ddc9f7bcf796b3d2
ulkish <dev.hugomoran@gmail.com>
2022-02-28 16:38:39 -0300

    Add a sg, lb listener, lb target group.

--- main.tf
+++ main.tf
@@ -8,9 +8,9 @@ variable "server_port" {
 	default     = 8080
 }
 
-output "public_ip" {
-	value = aws_instance.example.public_ip
-	description = "The public IP address of the web server"
+output "alb_dns_name" {
+	value = aws_lb.example.dns_name
+	description = "The domain name of the load balancer"
 }
 
 data "aws_vpc" "default" {
@@ -42,6 +42,9 @@ resource "aws_autoscaling_group" "example" {
 	launch_configuration = aws_launch_configuration.example
 	vpc_zone_identifier  = data.aws_subnet_ids.default.ids
 
+	target_group_arns = [aws_lb_target_group.asg.arn]
+	health_check_type = "ELB"
+
 	min_size             = 2
 	max_size             = 10
 
@@ -67,6 +70,7 @@ resource "aws_lb" "example" {
 	name               = "terraform-asg-example"
 	load_balancer_type = "application"
 	subnets            = data.aws_subnet_ids.default.ids
+	security_groups    = [aws_security_group.alb.id]
 }
 
 resource "aws_lb_listener" "http" {
@@ -81,5 +85,51 @@ resource "aws_lb_listener" "http" {
 			message_body = "404: page not found"
 			status_code  = 404
 		}
+	}
+}
+
+resource "aws_security_group" "alb" {
+	name = "terraform-example-alb"
+	# Allow inbound HTTP requests
+	ingress {
+		from_port   = 80
+		to_port     = 80
+		protocol    = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	# Allow all outbound requests
+	egress {
+		from_port   = 0
+		to_port     = 0
+		protocol    = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
+
+resource "aws_lb_target_group" "asg" {
+	name     = "terraform-asg-example"
+	port     = var.server_port
+	protocol = "HTTP"
+	vpc_id   = data.aws_vpc.default.idhealth_check {
+		path                = "/"
+		protocol            = "HTTP"
+		matcher             = "200"
+		interval            = 15
+		timeout             = 3
+		healthy_threshold   = 2
+		unhealthy_threshold = 2
+	}
+}
+
+resource "aws_lb_listener_rule" "asg" {
+	listener_arn = aws_lb_listener.http.arn
+	priority     = 100
+	condition {
+		field  = "path-pattern"
+		values = ["*"]
+	}
+	action {
+		type = "forward"
+		target_group_arn = aws_lb_target_group.asg.arn
 	}
 }
