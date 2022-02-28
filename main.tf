ae85c65a47d1ce976fec30460025a1a392c3f31c
ulkish <dev.hugomoran@gmail.com>
2022-02-28 14:14:39 -0300

    Add a asg and a launch config.

--- main.tf
+++ main.tf
@@ -13,36 +13,32 @@ output "public_ip" {
 	description = "The public IP address of the web server"
 }
 
-resource "aws_instance" "example" {
-	ami                    = "ami-0c55b159cbfafe1f0"
-	instance_type          = "t2.micro"
-	vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_launch_configuration" "example" {
+	image_id        = "ami-0c55b159cbfafe1f0"
+	instance_type   = "t2.micro"
+	security_groups = [aws_security_group.instance.id]
 	user_data = <<-EOF
-				#!bin/bash
-				echo "Hello, World!" > index.html
-				nohup busybox httpd -f -p 8080 &
-				EOF
-
-	tags = {
-		Name = "terraform-example"
-	}
-
-}
-
-resource "aws_instance" "example_second" {
-	ami                    = "ami-0c55b159cbfafe1f0"
-	instance_type          = "t2.micro"
-	vpc_security_group_ids = [aws_security_group.instance.id]
-	user_data = <<-EOF
-				#!bin/bash
-				echo "Hello, World!" > index.html
+				#!/bin/bash
+				echo "Hello, World" > index.html
 				nohup busybox httpd -f -p ${var.server_port} &
 				EOF
 
-	tags = {
-		Name = "terraform-second"
+	# Required when using a launch configuration with an auto scaling group.
+	# https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+	lifecycle {
+		create_before_destroy = true
 	}
+}
 
+resource "aws_autoscaling_group" "example" {
+	launch_configuration = aws_launch_configuration.example.name
+	min_size             = 2
+	max_size             = 10
+	tag {
+		key = "Name"
+		value = "terraform-asg-example"
+		propagate_at_launch = true
+	}
 }
 
 resource "aws_security_group" "instance" {
